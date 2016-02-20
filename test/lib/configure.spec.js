d727d41c7ee4d50315b52757306e85cbff49e587
ryym <ryym.64@gmail.com>
2016-02-20 11:41:01 +0900

    :sunny::lock: Enable to pre-configure some paths

---
+++ test/lib/configure.spec.js
@@ -0,0 +1,46 @@
+import assert from 'power-assert';
+import sinon from 'sinon';
+import configure from '$lib/configure';
+
+/** @test {configure} */
+describe('configure()', () => {
+  let othernpm, runnpm;
+
+  function configureWith(config) {
+    return configure({
+      config,
+      io: {},
+      othernpm: () => othernpm
+    });
+  }
+
+  beforeEach(() => {
+    runnpm = sinon.spy();
+    othernpm = sinon.stub().returns(runnpm);
+  });
+
+  it('adds methods to `onpm` corresponding to configs', () => {
+    const config = {
+      examples: './examples',
+      site: './site'
+    };
+    const onpm = configureWith(config);
+
+    assert(
+      typeof onpm.examples === 'function' &&
+      typeof onpm.site === 'function'
+    );
+  });
+
+  describe('configured methods', () => {
+    it('run npm commands in the configured directory', () => {
+      const onpm = configureWith({ site: './site' });
+      onpm.site('command');
+      assert.deepEqual(
+        [othernpm.args[0], runnpm.args[0]],
+        [['./site'], ['command']]
+      );
+    });
+  });
+
+});
