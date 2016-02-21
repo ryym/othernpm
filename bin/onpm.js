f632e451e18bb80c88dfcf848773df011e0e450d
ryym <ryym.64@gmail.com>
2016-02-21 22:14:50 +0900

    :sunny: bin: Show usage if necessary

--- bin/onpm.js
+++ bin/onpm.js
@@ -2,6 +2,11 @@
 
 'use strict';
 
+if (needHelp(process.argv)) {
+  showUsage();
+  return;
+}
+
 var onpm = require('../build');
 var pathOrAlias = process.argv[2];
 var command = process.argv.splice(3);
@@ -13,4 +18,26 @@ if (onpm.hasOwnProperty(pathOrAlias)) {
 else {
   const path = pathOrAlias;
   onpm(path)(command);
+}
+
+/**
+ * Return true if arguments are invalid
+ * or it requires help messages.
+ */
+function needHelp(argv) {
+  if (argv.length < 4) {
+    return true;
+  }
+  var command = argv[2];
+  switch (command) {
+  case '-h':
+  case '--help':
+    return true;
+  default:
+    return false;
+  }
+}
+
+function showUsage() {
+  console.log('Usage: onpm [path|alias] [npm command]');
 }
